f3739248dbe811c08a032ebeb37d663a8ad8400c
Tri Nguyen <tringuyenminh92@gmail.com>
2015-03-08 00:35:22 +0700

    Research Modal to show message

--- B2B.PresentationLayer/Scripts/Custom/MyApp.js
+++ B2B.PresentationLayer/Scripts/Custom/MyApp.js
@@ -1,22 +1,66 @@
 ﻿
 (function () {
 
-    angular.module("GlobalModule", ['ui.bootstrap', 'ngTouch', 'ui.grid', 'ui.grid.pagination', 'ui.grid.edit', 'ui.grid.resizeColumns', 'ui.grid.selection', 'ui.grid.moveColumns', 'ui.grid.saveState','ui.bootstrap']);
+    angular.module("GlobalModule", ['ui.bootstrap', 'ngTouch', 'ui.grid', 'ui.grid.pagination', 'ui.grid.edit', 'ui.grid.resizeColumns',
+                                    'ui.grid.selection', 'ui.grid.moveColumns', 'ui.grid.saveState', 'ui.bootstrap']);
+
 })();
 
+//Alert class to show error message
 function Alert(element, message, type, position, size, delayTime) {
 
-    position = typeof position !== 'undefined' ? position : "right";
-    size = typeof size !== 'undefined' ? delayTime : "4";
-    delayTime = typeof delayTime !== 'undefined' ? delayTime : 3000;
+    position = position || "right";
+    size = delayTime || "4";
+    delayTime = delayTime || 3000;
     var selft = $("#" + element);
 
     this.ShowAlert = function () {
         $(selft).removeClass();
-        $(selft).addClass("pull-"+position);
-        $(selft).addClass("col-md-"+size);
+        $(selft).addClass("pull-" + position);
+        $(selft).addClass("col-md-" + size);
         $(selft).addClass("alert alert-" + type);
         $(selft).text(message);
         $(selft).fadeIn().delay(delayTime).fadeOut();
     }
+}
+
+// Controller xu ly cac thao tac cua message Modal
+angular.module("GlobalModule").controller("messageModalController", MessageModalController);
+MessageModalController.$inject = ['$scope', '$modalInstance', 'data'];
+function MessageModalController($scope, $modalInstance, data) {
+
+    $scope.data = data;
+
+    $scope.ok = function () {
+        //Close and Pass return result
+        $modalInstance.close();
+    };
+    //$scope.cancel = function () {
+    //    $modalInstance.dismiss('cancel');
+    //};
+
+}
+
+//Controller goi modal va truyen input
+angular.module("GlobalModule").controller("callingModalController", CallingModalController);
+CallingModalController.$inject = ['$scope', '$modal'];
+function CallingModalController($scope, $modal) {
+
+    $scope.showMessageError = function (size, template) {
+
+        var modalInstance = $modal.open({
+            templateUrl: template,
+            controller: 'messageModalController',
+            size: size,
+            resolve: {
+                data: function () { return { Title: 'abc', Content: 'acdss' } }
+            }
+        });
+        //modalInstance.result.then(function (OKData) {
+        //    //Read result of modal when user click Ok in modal and set to $scope.selected
+        //    $scope.selected = OKData;
+        //}, function (cancelData) {
+        //    $scope.selected = cancelData;
+        //});
+    };
 }
