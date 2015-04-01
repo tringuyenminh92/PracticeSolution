d1bb04a05d285c9d3307a4ffe54effe924909abd
hoangtintg93 <hoangtintg93@gmail.com>
2015-04-01 13:44:49 +0700

    Login DSHanghoa

---
+++ B2B.PresentationLayer/Scripts/Custom/LoginController.js
@@ -0,0 +1,34 @@
+﻿/// <reference path="MyApp.js" />
+
+angular.module("GlobalModule").controller("loginController", LoginController);
+
+LoginController.$inject = ['$scope', '$http'];
+function LoginController($scope, $http, $modalInstance) {
+    $scope.$scope = $scope;
+    $scope.myAccount = { username: "", password: "" };
+    //$scope.User = { UserId: "", _Username: "", _Password: "", NhanvienId: "", Active: "", Step: "", Version: "" };
+    $scope.checkLogin=function()
+    {
+        $http.post("CheckLogin", { user: $scope.myAccount.username, password: $scope.myAccount.password }).success(function (data, status, headers, config) {
+            if (data) {
+                if (data.result) {
+                    alert("Success!");
+                    //Chuyen trang neu thanh cong
+                    window.location.href='/Quote';
+                }
+                else {
+                    alert("Fail");                    
+                }
+            }
+
+        }).error(function (data, status, headers, config) {
+            alert("Error");
+        });
+    }
+    //$scope.checkLoginUser=function()
+    //{
+    //    $http.post("CheckLogin",{User:$scope.User}).success(function(data))
+    //}
+}
+
+
